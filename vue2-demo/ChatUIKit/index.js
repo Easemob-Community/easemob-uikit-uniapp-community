c5060b584b70ec5916a631c9461779959a4c85ad
HuangFeiPeng <1094521084@qq.com>
2026-03-06 18:44:26 +0800

    fix: 登录后自动加载联系人列表和群组列表

--- vue2-demo/ChatUIKit/index.js
+++ vue2-demo/ChatUIKit/index.js
@@ -232,9 +232,11 @@ class ChatUIKit {
     // 加载会话列表（首次从服务器获取）
     this.store.dispatch('conversation/getServerConversations')
     
-    // 后续可添加：加载联系人列表、群组列表等
-    // this.store.dispatch('contact/getContacts')
-    // this.store.dispatch('group/getGroupList')
+    // 加载联系人列表
+    this.store.dispatch('contact/getContactsFromServer')
+    
+    // 加载群组列表
+    this.store.dispatch('group/getJoinedGroupList')
   }
 
   /**
